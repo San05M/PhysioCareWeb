044529d1a44c1fa98eb0d394d9b6ecc65328bd73
Sandra Moya Ortega <san.mo.dra5@gmail.com>
2024-12-12 13:56:15 +0100

    add bootstrap

--- index.js
+++ index.js
@@ -16,12 +16,17 @@ mongoose.connect('mongodb://127.0.0.1:27017/physiocare');
 /* Inicializar Express */
 let app = express();
 
+/* Carga estática*/
+app.use(express.static(__dirname + '/node_modules/bootstrap/dist'));
+app.use('/public', express.static(__dirname + '/public'));
+
 /* Cargar middleware para peticiones POST y PUT
  * y enrutadores */
 app.use(express.json());
 app.use('/patients', patients);
 app.use('/records', records);
 app.use('/physios', physios);
+
 
 /* Puesta en marcha del servidor. Usamos variables de entorno para mayor seguridad. */
 console.log(process.env.PUERTO)
