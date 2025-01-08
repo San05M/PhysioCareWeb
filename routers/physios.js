b3543fce791044534f8c954e504ed985339d3907
Sandra Moya Ortega <san.mo.dra5@gmail.com>
2025-01-08 16:34:41 +0100

    Part 1 - Physio done

--- routers/physios.js
+++ routers/physios.js
@@ -1,187 +1,105 @@
 const express = require("express");
-
 let Physio = require(__dirname + "/../models/physio.js");
-const User = require(__dirname + "/../models/user.js");
 
 let router = express.Router();
 
-/* Obtener un listado de todos los pacientes */
+// Ejercicio: Middleware para mostrar información de la petición recibida
+router.use((req, res, next) => {
+  console.log(new Date().toString(), "Método:", req.method);
+  ", URL:", req.baseUrl;
+  next();
+});
+
+/* Obtener un listado de todos s */
 router.get("/", (req, res) => {
   Physio.find()
     .then((resultado) => {
+      res.status(200).render("physios/physios_list", { physios: resultado });
+    })
+    .catch((error) => {
+      res.render("error", { error: "Error registrando libro" });
+    });
+});
+
+// Buscar para editar
+router.get("/editar/:id", (req, res) => {
+  Physio.findById(req.params["id"])
+    .then((resultado) => {
       if (resultado) {
-        res
-          .status(200) // Código. Devuelve una lista con la información de los physio.
-          .send({ ok: true, result: resultado });
+        res.render("physios/physio_edit", { physio: resultado });
       } else {
-        res
-          .status(404) // Error 404. No hay physio.
-          .send({ ok: false, result: "Error. No hay physio." });
+        res.render("error", { error: "Physio no encontrado" });
       }
     })
     .catch((error) => {
-      res
-        .status(500) // Error 500. Mensaje indicando el error.
-        .send({ ok: false, error: "Error obteniendo physio." });
+      res.render("error", { error: "Phys no encontrado 2" });
     });
 });
 
-router.get("/find", (req, res) => {
-  console.log(req.query)
-  Physio.find({
-    specialty: { $regex: req.query.specialty },
-  })
-    .then((resultado) => {
-      if (resultado) res.status(200).send({ result: resultado });
-      else
-        res.status(404).send({
-          ok: false,
-          error: "Error. No se han obtenido fisio con esos criterios.",
-        });
-    })
-    .catch((error) => {
-      res
-        .status(500)
-        .send({ ok: false, error: error + " Error interno del servidor." });
-    });
-});
-
-/* Servicio de listado por id de un paciente en específico */
+/* Servicio de listado por id de en específico */
 router.get("/:id", (req, res) => {
-  Physio.findById(req.params.id)
+  Physio.findById(req.params["id"])
     .then((resultado) => {
-      if (resultado)
-        res.status(200).send({
-          ok: true,
-          result: resultado,
-        });
-      // Código 200. Información del paciente.
-      else
-        res
-          .status(404) // Error 404. No hay pacientes.
-          .send({ ok: false, error: "No se han encontrado Physio" });
+      if (resultado) {
+        res.render("physios/physio_detail", { physio: resultado });
+      } else {
+        res.render("error", { error: "physio no encontrado" });
+      }
     })
     .catch((error) => {
-      res
-        .status(500) // Error 500. Mensaje indicando el error servidor.
-        .send({ ok: false, error: error + " Error interno del sevidor." });
+      res.render("error", { error: "Error buscando physio" });
     });
 });
 
-/* Buscar un physio por nombre o apellido */
-router.get(
-  "/find",
-  (req, res) => {
-    Physio.find({
-      surname: { $regex: req.query.surname, $options: "i" },
+/* Se añadirá que se reciba en la petición a la coleccións. */
+router.post("/", (req, res) => {
+  let newPhysio = new Libro({
+    name: req.body.name,
+    surname: req.body.surname,
+    specialty: req.body.address,
+    licenseNumber: req.body.insuranceNumber,
+  });
+  newPhysio
+    .save()
+    .then((resultado) => {
+      res.redirect(req.baseUrl);
     })
-      .then((resultado) => {
-        if (resultado) res.status(200).send({ result: resultado });
-        else
-          res.status(404).send({
-            ok: false,
-            error: "Error. No se han obtenido Physio con esos criterios.",
-          });
-      })
-      .catch((error) => {
-        res
-          .status(500)
-          .send({ ok: false, error: error + " Error interno del servidor." });
-      });
-  }
-);
-
-/* Se añadirá el physio que se reciba en la petición a la colección de pacientes. */
-router.post("/",  async (req, res) => {
-  try {
-    console.log(req.body)
-    const hash = bcrypt.hashSync(req.body.password, 10);
-
-    let usuario = new User({
-      login: req.body.login,
-      password: hash,
-      rol: "physio",
+    .catch((error) => {
+      res.render("error", { error: "Error añadiendo Phys" });
     });
-
-    /* Esperamos a obtener el usuario y se le otorga una id. */
-    const usuarioObtenido = await usuario.save();
-    /* Creación del id */
-    const id = usuarioObtenido._id;
-
-    console.log({
-      login: req.body.login,
-      password: hash,
-      name: req.body.name,
-      surname: req.body.surname,
-      specialty: req.body.specialty,
-      licenseNumber: req.body.licenseNumber,
-      rol: "physio",
-    })
-
-    /* Creación de usuario */
-    let physio = new Physio({
-      _id: id,
-
-      name: req.body.name,
-      surname: req.body.surname,
-      specialty: req.body.specialty,
-      licenseNumber: req.body.licenseNumber,
-    });
-
-    /* Esperamos a obtener el usuario y se le otorga una id. */
-    await physio.save();
-
-    res.status(201).send({ result: physio });
-  } catch (error) {
-    console.log(error)
-    res
-      .status(400)
-      .send({ ok: false, error: error + "Error al insertar un physio." });
-  }
 });
 
-/* Actualizar los datos a un physio. Revisar este. */
-router.put("/:id", async (req, res) => {
-  try {
-    const resultado = await Physio.findByIdAndUpdate(req.params.id, {
+/* Actualizar los datos a. Revisar este. */
+router.post("/:id", (req, res) => {
+  Physio.findByIdAndUpdate(
+    req.params.id,
+    {
       $set: {
         name: req.body.name,
         surname: req.body.surname,
-        speciality: req.body.speciality,
-        licenseNumber: req.body.licenseNumber,
+        specialty: req.body.address,
+        licenseNumber: req.body.insuranceNumber,
       },
+    },
+    { new: true }
+  )
+    .then((resultado) => {
+      res.redirect(req.baseUrl);
+    })
+    .catch((error) => {
+      res.render("error", { error: "Error modificando Phys." });
     });
-    if (resultado) {
-      res.status(200).send({ result: resultado });
-    } else {
-      res.status(400).send({
-        ok: false,
-        error: "Error actualizando los datos del Physio",
-      });
-    }
-  } catch (error) {
-    res.status(500).send({ ok: false, error: error + "Error en el servidor." });
-  }
 });
 
-/* Para borrar un physio por id. */
-router.put("/:id", async (req, res) => {
-  try {
-    await Physio.findByIdAndDelete(req.params.id).then((resultado) => {
-      if (resultado) {
-        User.findByIdAndDelete(req.params.id).then((resultadoId) => {
-          res.status(200).send({ result: resultadoId });
-        });
-      } else {
-        res.status(404).send({
-          ok: false,
-          error: "Error. El physio no existe.",
-        });
-      }
+// Borrado
+router.delete("/:id", (req, res) => {
+  Physio.findByIdAndRemove(req.params.id)
+    .then((resultado) => {
+      res.redirect(req.baseUrl);
+    })
+    .catch((error) => {
+      res.render("error", { error: "Error borrando physio" });
     });
-  } catch (error) {
-    res.status(500).send({ ok: false, error: error + "Error en el servidor." });
-  }
 });
 
 module.exports = router;
